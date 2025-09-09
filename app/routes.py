b013fa9b20e8a535a7aef99d8eb1e7b51c5393c4
Gabrielli Araujo <gabisaraujo0707@gmail.com>
2025-09-09 09:59:42 -0300

    Listando imoveis e devolvendo em dicionario json, tdd e funcionalidade passando
    Co-authored-by: CoutinhoRf <CoutinhoRf@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,31 @@
 from flask import Blueprint, jsonify
+from .db import get_connection
 
 bp = Blueprint('routes', __name__)
 
 @bp.route('/')
 def home():
     return jsonify({"message": "API Imobiliária rodando!"})
+
+@bp.route('/imoveis')
+def listar_imoveis():
+    con = get_connection()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM imoveis")
+    rows = cursor.fetchall() #todas as linhas
+    con.close()
+    
+    # Pega os nomes das colunas da tabela (id, logradouro, cidade, etc.)
+    colunas = []
+    for desc in cursor.description:
+        colunas.append(desc[0]) #adiciona os nomes das colunas 
+
+    # Agora cria a lista de imóveis como dicionários
+    imoveis = []
+    for row in rows:
+        item = {}
+        for i, valor in enumerate(row):
+            nome_coluna = colunas[i]
+            item[nome_coluna] = valor
+        imoveis.append(item)
+    return jsonify(imoveis) #retorna dados em formato json
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,6 @@
 import pytest
 from app import create_app
+import flask
 
 @pytest.fixture
 def client():
@@ -11,3 +12,12 @@ def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.get_json() == {"message": "API Imobiliária rodando!"}
+
+def teste_lista_imoveis(client):
+    response = client.get('/imoveis')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list)
+    assert len(data) > 0
+    assert 'id' in data[0] #verificar no primeiro se existir
+    assert 'cidade' in data[0]
