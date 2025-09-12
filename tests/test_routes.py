c54a219e951129c7584e820bbff2ed641e581bbb
Rafaela Coutinho <rafaelamc4@al.insper.edu.br>
2025-09-12 16:18:44 -0300

    teste add imoveis

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,6 @@
 import pytest
 from app import create_app
+from unittest.mock import patch, MagicMock
 
 @pytest.fixture
 def client():
@@ -11,3 +12,25 @@ def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.get_json() == {"message": "API Imobiliária rodando!"}
+
+def test_add_imoveis(client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+        
+    novo_imovel = {
+        "logradouro": "Rua das Flores",
+        "tipo_logradouro": "Rua",
+        "bairro": "Centro",
+        "cidade": "São Paulo",
+        "cep": "01000-000",
+        "tipo": "Apartamento",
+        "valor": 500000.00,
+        "data_aquisicao": "2023-05-10"
+    }
+    
+    response = client.post('/add')
+    assert response.status_code == 201
+    data = response.get_json() #pega a resposta de json do programa(mensagem q voltara)
+    assert data['message'] == 'alguma Coisa aconteceu' #faz a mensagem voltar desse jeito
+    
