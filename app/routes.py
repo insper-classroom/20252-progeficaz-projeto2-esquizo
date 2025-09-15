210e3b27c4ae12dad81f5831f3c02ba11551cab2
Rafaela Coutinho <rafaelamc4@al.insper.edu.br>
2025-09-15 19:07:37 -0300

    listando cidades com molde de lista de imoveis

--- app/routes.py
+++ app/routes.py
@@ -109,3 +109,25 @@ def update_imoveis(id):
         
     return jsonify({"message": "Imóvel alterado com sucesso"}), 200   
 
+@bp.route('/imoveis/cidade=<cidade>', methods=['GET'])
+def list_cidades():
+    con = get_connection()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM imoveis WHERE cidade = %s")
+    rows = cursor.fetchall() #todas as linhas
+    con.close()
+    
+    # Pega os nomes das colunas da tabela (id, logradouro, cidade, etc.)
+    colunas = []
+    for desc in cursor.description:
+        colunas.append(desc[0]) #adiciona os nomes das colunas 
+
+    # Agora cria a lista de imóveis como dicionários
+    imoveis = []
+    for row in rows:
+        item = {}
+        for i, valor in enumerate(row):
+            nome_coluna = colunas[i]
+            item[nome_coluna] = valor
+        imoveis.append(item)
+    return jsonify(imoveis) #retorna dados em formato json
